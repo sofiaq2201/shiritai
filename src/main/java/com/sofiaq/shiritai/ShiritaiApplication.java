f76a213d31ebfbe727f0f25489f9f4cee81ec46f
sofiaq2201 <sofiaq2201@gmail.com>
2020-09-01 21:52:40 -0300

    Initalizing proyect

---
+++ src/main/java/com/sofiaq/shiritai/ShiritaiApplication.java
@@ -0,0 +1,13 @@
+package com.sofiaq.shiritai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiritaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiritaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofiaq/shiritai/ShiritaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofiaq.shiritai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiritaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
